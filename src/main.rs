24ab6b90602d709b6861bfd202d27fba2c2cc8ff
DefectingCat <i@defect.ink>
2023-02-08 16:24:23 +0800

    Listen TCP connection for localhost

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,9 @@
+use std::net::TcpListener;
+
 fn main() {
-    println!("Hello, world!");
+    let listener  = TcpListener::bind("127.0.0.1:4000").expect("cannon listen on port 4000");
+    for stream in listener.incoming() {
+        let stream = stream.unwrap();
+        println!("Connection established!");
+    }
 }
