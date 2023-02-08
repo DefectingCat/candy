a0136094fd451ddd9d751f6bf588ecc742f50d87
DefectingCat <i@defect.ink>
2023-02-08 16:55:13 +0800

    Read HTTP client infomation.

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,21 @@
-use std::net::TcpListener;
+use std::io::{BufRead, BufReader};
+use std::net::{TcpListener, TcpStream};
 
 fn main() {
-    let listener  = TcpListener::bind("127.0.0.1:4000").expect("cannon listen on port 4000");
+    let listener = TcpListener::bind("127.0.0.1:4000").expect("cannon listen on port 4000");
     for stream in listener.incoming() {
         let stream = stream.unwrap();
-        println!("Connection established!");
+        handle_connection(&stream);
     }
+}
+
+fn handle_connection(mut stream: &TcpStream) {
+    let buf_reader = BufReader::new(&mut stream);
+    let http_request: Vec<_> = buf_reader
+        .lines()
+        .map(|result| result.unwrap())
+        .take_while(|line| !line.is_empty())
+        .collect();
+
+    dbg!(&http_request);
 }
