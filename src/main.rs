612747d9f2262ac5caf945a60ab7ec804e5d536e
DefectingCat <i@defect.ink>
2023-02-09 09:33:24 +0800

    Collect user request's headers

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,8 @@
+use std::collections::HashMap;
 use std::fs;
-use std::io::{BufRead, BufReader, Write};
+use std::io::{BufRead, BufReader, Read, Write};
 use std::net::{TcpListener, TcpStream};
+use std::ptr::hash;
 
 fn main() {
     let listener = TcpListener::bind("127.0.0.1:4000").expect("cannon listen on port 4000");
@@ -12,15 +14,33 @@ fn main() {
 
 fn handle_connection(mut stream: &TcpStream) {
     let buf_reader = BufReader::new(&mut stream);
-    // @TODO Use Content-Length to read post body.
-    let http_request: Vec<_> = buf_reader
+    let msg: Vec<_> = buf_reader.bytes().map(|byte| byte.unwrap()).collect();
+    let request: Vec<_> = msg
         .lines()
-        .map(|result| result.unwrap())
+        .map(|line| line.unwrap())
         .take_while(|line| !line.is_empty())
         .collect();
-    let first_line = &http_request[0];
-
-    dbg!(&http_request);
+    let mut headers = HashMap::new();
+    request.iter().for_each(|header| {
+        if let Some(head) = header.split_once(": ") {
+            headers.entry(head.0).or_insert(head.1);
+        }
+    });
+    dbg!(&headers);
+    let str = String::from_utf8_lossy(&msg);
+    // buf_reader.lines().for_each(|result| {
+    //     let result = result.unwrap();
+    //     dbg!(&result);
+    // });
+    // @TODO Use Content-Length to read post body.
+    // let http_request: Vec<_> = buf_reader
+    //     .lines()
+    //     .map(|result| result.unwrap())
+    //     .take_while(|line| !line.is_empty())
+    //     .collect();
+    // let first_line = &http_request[0];
+    //
+    // dbg!(&http_request);
 
     let status_line = "HTTP/1.1 200 OK";
     let contents = fs::read_to_string("./static/index.html").unwrap();
