bf44553d51ccf3ceebe012c6416321281fd0acf9
DefectingCat <i@defect.ink>
2023-02-09 11:40:20 +0800

    Add thread and works

--- src/thread_pool.rs
+++ src/thread_pool.rs
@@ -1,15 +1,33 @@
 use std::thread;
 
 pub struct ThreadPool {
-    thread_num: u32,
-    threads: Vec<thread::JoinHandle<()>>,
+    pub thread_num: usize,
+    pub works: Vec<Worker>,
 }
 
 impl ThreadPool {
-    fn new(thread_num: u32) -> Self {
+    pub fn new(thread_num: usize) -> Self {
+        let mut works = Vec::with_capacity(thread_num);
+        for id in 0..thread_num {
+            works.push(Worker::new(id))
+        }
         Self {
             thread_num,
-            threads: vec![],
+            works: vec![],
+        }
+    }
+}
+
+pub struct Worker {
+    pub id: usize,
+    thread: thread::JoinHandle<()>,
+}
+
+impl Worker {
+    pub fn new(id: usize) -> Self {
+        Self {
+            id,
+            thread: thread::spawn(|| {}),
         }
     }
 }
