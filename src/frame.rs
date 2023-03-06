0dfe48049c8a0299fcf2d022ff24784e36df1c35
DefectingCat <i@defect.ink>
2023-03-06 17:59:20 +0800

    Add frame error

--- src/frame.rs
+++ src/frame.rs
@@ -2,9 +2,36 @@ use anyhow::Result;
 use log::error;
 use std::{
     collections::HashMap,
+    error::Error,
+    fmt::Display,
     io::{BufRead, BufReader},
     net::TcpStream,
 };
+
+#[derive(Debug)]
+pub struct FrameError {
+    details: String,
+}
+
+impl FrameError {
+    fn new(msg: &str) -> Self {
+        Self {
+            details: msg.to_string(),
+        }
+    }
+}
+
+impl Display for FrameError {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "{}", self.details)
+    }
+}
+
+impl Error for FrameError {
+    fn description(&self) -> &str {
+        return &self.details;
+    }
+}
 
 pub struct HttpFrame<'a> {
     pub request_str: String,
@@ -14,8 +41,14 @@ pub struct HttpFrame<'a> {
 }
 
 impl<'a> HttpFrame<'a> {
-    fn new(reader: &mut BufReader<&mut &TcpStream>) -> Result<Self> {
-        let mut request_str = read_request(reader)?;
+    fn new(reader: &mut BufReader<&mut &TcpStream>) -> Result<Self, FrameError> {
+        let mut request_str = match read_request(reader) {
+            Ok(str) => str,
+            Err(err) => {
+                error!("{}", err.description());
+                Err(FrameError(&err.description()));
+            }
+        };
 
         // Read string to lines.
         let request: Vec<_> = request_str.lines().collect();
@@ -24,16 +57,16 @@ impl<'a> HttpFrame<'a> {
             Some(res) => *res,
             None => {
                 error!("failed to parse request method");
-                return handle_error(stream);
+                return Err(FrameError::new("failed to parse request method"));
             }
         };
 
-        Self {
+        Ok(Self {
             request_str,
             headers: (),
             method: (),
             path: (),
-        }
+        })
     }
 }
 
