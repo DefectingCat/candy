b13ff3b5a552dfad33b9482f60c84615b8fbac54
DefectingCat <i@defect.ink>
2023-03-07 15:29:40 +0800

    Add frame info

--- src/frame.rs
+++ src/frame.rs
@@ -33,20 +33,20 @@ impl Error for FrameError {
     }
 }
 
-pub struct HttpFrame<'a> {
-    pub request_str: String,
+pub struct HttpFrame {
+    request_str: String,
     pub headers: HashMap<String, String>,
-    pub method: &'a str,
-    pub path: &'a str,
+    pub first_line: String,
+    pub router: HashMap<&'static str, Option<String>>,
 }
 
-impl<'a> HttpFrame<'a> {
+impl HttpFrame {
     fn new(reader: &mut BufReader<&mut &TcpStream>) -> Result<Self, FrameError> {
-        let mut request_str = match read_request(reader) {
+        let request_str = match read_request(reader) {
             Ok(str) => str,
             Err(err) => {
-                error!("{}", err.description());
-                Err(FrameError(&err.description()));
+                error!("{:?}", err);
+                return Err(FrameError::new(&err.to_string()));
             }
         };
 
@@ -54,24 +54,38 @@ impl<'a> HttpFrame<'a> {
         let request: Vec<_> = request_str.lines().collect();
         // HTTP method in first line.
         let first_line = match request.first() {
-            Some(res) => *res,
+            Some(res) => String::from(*res),
             None => {
                 error!("failed to parse request method");
                 return Err(FrameError::new("failed to parse request method"));
             }
         };
 
+        let headers = collect_headers(&request);
+
+        let mut router: HashMap<&'static str, Option<String>> = HashMap::new();
+        let first_line = first_line.clone();
+        let mut inline_first_line: Vec<_> =
+            first_line.split(' ').map(|txt| String::from(txt)).collect();
+
+        if let Some(method) = inline_first_line.first() {
+            router.insert("method", Some(method.clone()));
+        }
+        if let Some(path) = inline_first_line.get(1) {
+            router.insert("path", Some(path.clone()));
+        }
+
         Ok(Self {
             request_str,
-            headers: (),
-            method: (),
-            path: (),
+            headers,
+            first_line,
+            router,
         })
     }
 }
 
 /// Read http request to string.
-pub fn read_request(reader: &mut BufReader<&mut &TcpStream>) -> Result<String> {
+fn read_request(reader: &mut BufReader<&mut &TcpStream>) -> Result<String> {
     let mut request_string = String::new();
     loop {
         let byte = reader.read_line(&mut request_string)?;
@@ -80,4 +94,17 @@ pub fn read_request(reader: &mut BufReader<&mut &TcpStream>) -> Result<String> {
         }
     }
     Ok(request_string)
+}
+
+/// Collect request string with Hashmap to headers.
+pub fn collect_headers(request: &[&str]) -> HashMap<String, String> {
+    let mut headers = HashMap::new();
+    request.iter().for_each(|header| {
+        if let Some(head) = header.split_once(": ") {
+            headers
+                .entry(head.0.to_string())
+                .or_insert(head.1.to_string());
+        }
+    });
+    headers
 }
