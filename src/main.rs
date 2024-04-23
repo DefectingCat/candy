3f9e17bfd6694ea5633c05e2d01b2f2366c8c7a9
xfy <i@rua.plus>
2024-04-23 08:46:42 +0800

    add logger

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
-fn main() {
+use anyhow::Result;
+use tracing::info;
+
+use crate::utils::init_logger;
+
+mod utils;
+
+#[tokio::main]
+async fn main() -> Result<()> {
+    init_logger();
     println!("Hello World");
+    info!("Hello");
+    Ok(())
 }
