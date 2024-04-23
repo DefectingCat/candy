5d462c25e04a04ddbd88f481bd7308cbcea0276d
xfy <i@rua.plus>
2024-04-23 09:06:19 +0800

    add init config file

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,17 @@
-use anyhow::Result;
-use tracing::info;
+use anyhow::{Context, Result};
+use tracing::{debug, info};
 
-use crate::utils::init_logger;
+use crate::{config::init_config, utils::init_logger};
 
+mod config;
+mod error;
 mod utils;
 
 #[tokio::main]
 async fn main() -> Result<()> {
     init_logger();
+    let settings = init_config().with_context(|| "init config failed")?;
+    debug!("settings {:?}", settings);
     println!("Hello World");
     info!("Hello");
     Ok(())
