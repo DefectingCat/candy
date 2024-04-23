dce79be422e9b78ea3ca12ea0aae8109acf8cbd8
xfy <i@rua.plus>
2024-04-23 13:13:41 +0800

    implment host response

--- src/error.rs
+++ src/error.rs
@@ -2,10 +2,12 @@ use std::io;
 
 #[derive(thiserror::Error, Debug)]
 pub enum Error {
-    #[error("failed io")]
+    #[error("failed io {0}")]
     IoError(#[from] io::Error),
-    #[error("failed to decode toml")]
+    #[error("failed to decode toml {0}")]
     TomlDecode(#[from] toml::de::Error),
+    #[error("failed to handle http {0}")]
+    HttpError(#[from] hyper::http::Error),
 }
 
 pub type Result<T, E = Error> = anyhow::Result<T, E>;
