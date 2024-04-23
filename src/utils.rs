ef657381f0cbbab445e8bf396294bb68d5c1f795
xfy <i@rua.plus>
2024-04-23 08:46:42 +0800

    add logger

---
+++ src/utils.rs
@@ -0,0 +1,13 @@
+use tracing_subscriber::{fmt, prelude::*, registry, EnvFilter};
+
+pub fn init_logger() {
+    let formatting_layer = fmt::layer()
+        // .pretty()
+        .with_thread_ids(false)
+        .with_target(false)
+        .with_writer(std::io::stdout);
+
+    let env_layer = EnvFilter::try_from_env("CANDY_LOG").unwrap_or_else(|_| "info".into());
+
+    registry().with(env_layer).with(formatting_layer).init();
+}
