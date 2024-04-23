7a8d5ec3c805cb3514a879f3df80d6043c5eaeb0
xfy <i@rua.plus>
2024-04-23 17:20:03 +0800

    improve performance
    remove host clone
    
    parse static path

--- src/error.rs
+++ src/error.rs
@@ -3,11 +3,11 @@ use std::io;
 #[derive(thiserror::Error, Debug)]
 pub enum Error {
     #[error("failed io {0}")]
-    IoError(#[from] io::Error),
+    Io(#[from] io::Error),
     #[error("failed to decode toml {0}")]
     TomlDecode(#[from] toml::de::Error),
     #[error("failed to handle http {0}")]
-    HttpError(#[from] hyper::http::Error),
+    Http(#[from] hyper::http::Error),
 }
 
 pub type Result<T, E = Error> = anyhow::Result<T, E>;
--- src/utils.rs
+++ src/utils.rs
@@ -10,3 +10,16 @@ pub fn init_logger() {
     let env_layer = EnvFilter::try_from_env("CANDY_LOG").unwrap_or_else(|_| "info".into());
     registry().with(env_layer).with(formatting_layer).init();
 }
+
+pub fn find_static_path<'a>(req_path: &'a str, location: &str) -> Option<&'a str> {
+    let location_len = location.len();
+    if req_path.len() < location_len {
+        return None;
+    }
+    let path = &req_path[..location_len];
+    if path == location {
+        Some(&req_path[location_len - 1..])
+    } else {
+        None
+    }
+}
