bd9df5235c854eda4cf7c2dde4fe3b85d4976f01
xfy <i@rua.plus>
2024-04-26 11:45:07 +0800

    add keep-alive default timeout

--- src/consts.rs
+++ src/consts.rs
@@ -4,3 +4,9 @@ pub const NAME: &str = env!("CARGO_PKG_NAME");
 pub const VERSION: &str = env!("CARGO_PKG_VERSION");
 pub const OS: &str = env::consts::OS;
 pub const ARCH: &str = env::consts::ARCH;
+
+// config defaults
+pub const KEEP_ALIVE_TIMEOUTD_EFAULT: u16 = 75;
+pub fn keep_alive_timeoutd_efault() -> u16 {
+    KEEP_ALIVE_TIMEOUTD_EFAULT
+}
