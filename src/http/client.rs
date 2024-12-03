2605f26bf999cfb66efd4318924f5b79d9c6089e
xfy <i@rua.plus>
2024-12-03 09:27:40 +0800

    feat(proxy): follow client redirects
    follow 301 and 302 redirects

--- src/http/client.rs
+++ src/http/client.rs
@@ -1,29 +1,31 @@
+use std::str::FromStr;
+
+use anyhow::Context;
 use bytes::Bytes;
-use http::Uri;
+use http::{Response, Uri};
 use http_body_util::{combinators::BoxBody, BodyExt, Empty};
+use hyper::body::Incoming;
 use hyper_rustls::ConfigBuilderExt;
 use hyper_util::{client::legacy::Client, rt::TokioExecutor};
 
 use crate::error::Error;
 
-/// Get http response Body
-///
-/// ## Arguments
-///
-/// `url`: http url
+const MAX_REDIRECTS: usize = 10;
+
+/// Get http response
 ///
 /// ## Example
 ///
 /// ```rust
-/// use candy::http::client::get;
+/// use candy::http::client::get_inner;
 ///
-/// let body = get("https://www.google.com").await.unwrap();
+/// let res = get_inner("https://www.google.com").await.unwrap();
 /// ```
 ///
 /// ## Return
 ///
-/// `anyhow::Result<Bytes>`
-pub async fn get(url: Uri) -> anyhow::Result<BoxBody<Bytes, Error>> {
+/// `anyhow::Result<Response<Incoming>>`
+pub async fn get_inner(url: Uri) -> anyhow::Result<Response<Incoming>> {
     // let _ = rustls::crypto::ring::default_provider().install_default();
     let _ = rustls::crypto::aws_lc_rs::default_provider().install_default();
 
@@ -43,6 +45,42 @@ pub async fn get(url: Uri) -> anyhow::Result<BoxBody<Bytes, Error>> {
     // Build the hyper client from the HTTPS connector.
     let client: Client<_, Empty<Bytes>> = Client::builder(TokioExecutor::new()).build(https);
 
-    let res = client.get(url).await?.map_err(Error::HyperError).boxed();
+    let res = client.get(url).await?;
+    Ok(res)
+}
+
+/// Get http response Body
+/// And follow redirects
+///
+/// ## Example
+///
+/// ```rust
+/// use candy::http::client::get;
+///
+/// let body = get("https://www.google.com").await.unwrap();
+/// ```
+///
+/// ## Return
+///
+/// `anyhow::Result<Bytes>`
+pub async fn get(url: Uri) -> anyhow::Result<BoxBody<Bytes, Error>> {
+    let mut redirects = 0;
+
+    let mut res = get_inner(url).await?;
+    while (res.status() == 301 || res.status() == 302) && redirects < MAX_REDIRECTS {
+        redirects += 1;
+        let location = res
+            .headers()
+            .get("location")
+            .ok_or(Error::MissingHeader("location"))
+            .with_context(|| "missing header location")?
+            .to_str()
+            .with_context(|| "failed to convert header value to str")?
+            .to_string();
+        let url = Uri::from_str(&location).with_context(|| "failed to convert str to url")?;
+        res = get_inner(url).await?;
+    }
+
+    let res = res.map_err(Error::HyperError).boxed();
     Ok(res)
 }
