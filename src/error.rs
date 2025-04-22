c6ff8af73e51f9514dec9732fb182ae93968675a
xfy <i@rua.plus>
2025-04-22 15:23:46 +0800

    feat: add custom error message

--- src/error.rs
+++ src/error.rs
@@ -1,8 +1,7 @@
-use std::{borrow::Cow, io, num::TryFromIntError, sync::PoisonError, time::SystemTimeError};
+use std::{io, num::TryFromIntError, time::SystemTimeError};
 
-use anyhow::anyhow;
 use http::uri::InvalidUri;
-use hyper::header::{InvalidHeaderValue, ToStrError};
+use hyper::header::ToStrError;
 
 #[allow(clippy::enum_variant_names)]
 #[derive(thiserror::Error, Debug)]
@@ -24,24 +23,8 @@ pub enum Error {
     InvalidUri(#[from] InvalidUri),
     #[error("hyper {0}")]
     HyperError(#[from] hyper::Error),
-
-    // http
-    #[error("route not found {0}")]
-    NotFound(Cow<'static, str>),
     #[error("internal server error {0}")]
-    InternalServerError(#[from] anyhow::Error),
-    #[error("invalide header value {0}")]
-    InvalidHeader(#[from] InvalidHeaderValue),
-    #[error("")]
-    Empty,
-    #[error("missing header {0}")]
-    MissingHeader(&'static str),
+    Any(#[from] anyhow::Error),
 }
 
 pub type Result<T, E = Error> = anyhow::Result<T, E>;
-
-impl<T> From<PoisonError<T>> for Error {
-    fn from(err: PoisonError<T>) -> Self {
-        Self::InternalServerError(anyhow!("global cache poisoned {err}"))
-    }
-}
