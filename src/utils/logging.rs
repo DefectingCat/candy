d36dfbc2e280d10d74282ed2965aebf44fe14305
xfy <i@rua.plus>
2025-07-28 19:37:57 +0800

    chore: bump deps
    update mlua

--- src/utils/logging.rs
+++ src/utils/logging.rs
@@ -5,6 +5,12 @@ use tracing_subscriber::{
     registry,
 };
 
+/// 初始化 Logger
+///
+/// 从配置文件中读取 log 级别，同时读取日志文件存储路径。
+/// 无论是否设置了日志文件路径，都会将日志输出到控制台。
+///
+/// 配置文件路径只能文件夹，日志文件将按天分割。
 pub fn init_logger() {
     let formatting_layer = fmt::layer()
         // .pretty()
